69bdde10a304ff86ad18df54e5803360881ed5d1
Cassandra Yurekli <cassandra.yurekli@gmail.com>
2016-03-14 15:08:50 +0100

    Create Projet

---
+++ BAJunior/BAJunior/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAJunior.Model
+{
+    class User
+    {
+        private String login;
+        private String password;
+        private bool admin;
+
+
+    }
+}
---
+++ BAJunior/BAJunior/ServiceData/DbUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAJunior.ServiceData
+{
+    /// <summary>
+    /// Méthodes génériques pour la Base de données (initialisation de connection etc) 
+    /// </summary>
+    class DbUtils
+    {
+    }
+}
