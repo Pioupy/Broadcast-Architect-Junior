417530521d512df702e4c7cd74aab161586929b3
Cassandra Yurekli <cassandra.yurekli@gmail.com>
2016-03-14 15:20:36 +0100

    User et ButtonData

--- BAJunior/BAJunior/Model/User.cs
+++ BAJunior/BAJunior/Model/User.cs
@@ -8,10 +8,35 @@ namespace BAJunior.Model
 {
     class User
     {
-        private String login;
-        private String password;
-        private bool admin;
+        private String m_login;
+        private String m_password;
+        private bool m_admin;
 
+        // [Getter/Setter] String m_login
+        public String getLogin() {
+            return this.m_login;
+        }
+        public void setLogin(String login)
+        {
+            this.m_login = login;
+        }
 
+        // [Getter/Setter] String m_password
+        public String getPassword()
+        {
+            return this.m_password;
+        }
+        public void setPassword(String password)
+        {
+            this.m_password = password;
+        }
+
+        // [Getter/Setter] Boolean m_admin
+        public bool isAdmin() {
+            return this.m_admin;
+        }
+        public void setAdmin(bool admin) {
+            this.m_admin = admin;
+        }
     }
 }
