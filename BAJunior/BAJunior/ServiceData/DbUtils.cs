9b61989b69f02c0f57696123f89200c3e8635691
CaptainKenjy <goget.alexandre@gmail.com>
2016-03-14 16:07:14 +0100

    Mise à jour

--- BAJunior/BAJunior/ServiceData/DbUtils.cs
+++ BAJunior/BAJunior/ServiceData/DbUtils.cs
@@ -14,76 +14,22 @@ namespace BAJunior.ServiceData
     {
         string nameDatabase = "MyDatabase.sqlite";
         private SQLiteConnection m_dbConnection;
-        public void createDatabase(string pNameDatebase)
+        public void CreateDatabase(string pNameDatebase)
         {
             SQLiteConnection.CreateFile(pNameDatebase);
         }
-        public void openConnectionDatabase(string pNameDatabase)//pNameDatebase uitliser !!
+        public void OpenConnectionDatabase(string pNameDatabase)//pNameDatebase uitliser !!
         {
             m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
             m_dbConnection.Open();
         }
-        public void closeConnectionDatabase()
+        public void CloseConnectionDatabase()
         {
             m_dbConnection.Close();
         }
-        public Boolean createTable(string pSql)
+        public Boolean ExecuteQuery(string pSql)
         {
             //string sql = "create table highscores (name varchar(20), score int)";
-            SQLiteCommand command = new SQLiteCommand(pSql, m_dbConnection);
-            int row = command.ExecuteNonQuery();
-            if (row > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public Boolean selectTable(string pSql)
-        {
-            //string sql = "select * from highscores order by score desc";
-            SQLiteCommand command = new SQLiteCommand(pSql, m_dbConnection);
-            int row = command.ExecuteNonQuery();
-            if (row > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public Boolean insertTable(string pSql)
-        {
-            //string sql = "insert into highscores (name, score) values ('Me', 3000)";
-            SQLiteCommand command = new SQLiteCommand(pSql, m_dbConnection);
-            int row = command.ExecuteNonQuery();
-            if (row > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public Boolean updateTable(string pSql)
-        {
-            SQLiteCommand command = new SQLiteCommand(pSql, m_dbConnection);
-            int row = command.ExecuteNonQuery();
-            if (row > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public Boolean deleteTable(string pSql)
-        {
             SQLiteCommand command = new SQLiteCommand(pSql, m_dbConnection);
             int row = command.ExecuteNonQuery();
             if (row > 0)
