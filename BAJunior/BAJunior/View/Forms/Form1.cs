1abd9531004e8aaf6ab0e5ce8f54112c8bc55f0c
mctailleux <marieclaire.tailleux@gmail.com>
2016-03-16 09:28:53 +0100

    Création champs Login/ Mot de passe

--- BAJunior/BAJunior/View/Forms/Form1.cs
+++ BAJunior/BAJunior/View/Forms/Form1.cs
@@ -12,9 +12,85 @@ namespace BAJunior
 {
     public partial class Form1 : Form
     {
+        private Label Identifiant;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Identifiant = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Identifiant
+            // 
+            this.Identifiant.AutoSize = true;
+            this.Identifiant.Location = new System.Drawing.Point(206, 135);
+            this.Identifiant.Name = "Identifiant";
+            this.Identifiant.Size = new System.Drawing.Size(51, 20);
+            this.Identifiant.TabIndex = 0;
+            this.Identifiant.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(394, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(394, 214);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connexion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(980, 529);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Identifiant);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ BAJunior/BAJunior/View/Forms/user/Profils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAJunior.View.Forms.user
+{
+    public partial class Profils : Form
+    {
+        public Profils()
+        {
+            InitializeComponent();
+        }
+    }
+}
