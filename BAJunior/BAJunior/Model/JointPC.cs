6939e10cf2747ec8394242f1d28458406b9b5101
CaptainKenjy <goget.alexandre@gmail.com>
2016-04-10 12:28:38 +0200

    Ajout model

---
+++ BAJunior/BAJunior/Model/JointPC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAJunior.Model
+{
+    class JointPC
+    {
+        private String m_btnKeyboard;
+        private int m_idProfil;
+        private int m_idCommand;
+        // [Getter/Setter] String m_name
+        public String getBtnKeyboard()
+        {
+            return this.m_btnKeyboard;
+        }
+        public void setBtnKeyboard(String btnKeyboard)
+        {
+            this.m_btnKeyboard = btnKeyboard;
+        }
+        // [Getter/Setter] String m_idProfil
+        public int getIdProfil()
+        {
+            return this.m_idProfil;
+        }
+        public void setIdProfil(int idProfil)
+        {
+            this.m_idProfil = idProfil;
+        }
+        // [Getter/Setter] String m_idCommand
+        public int getIdCommand()
+        {
+            return this.m_idCommand;
+        }
+        public void setIdCommand(int idCommand)
+        {
+            this.m_idCommand = idCommand;
+        }
+    }
+}
---
+++ BAJunior/BAJunior/Model/Param.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAJunior.Model
+{
+    class Param
+    {
+        private String m_name;
+        private int m_idSetCommand;
+        private int m_idCommand;
+        // [Getter/Setter] String m_name
+        public String getName()
+        {
+            return this.m_name;
+        }
+        public void setName(String name)
+        {
+            this.m_name = name;
+        }
+        // [Getter/Setter] String m_idSetCommand
+        public int getIdSetCommand()
+        {
+            return this.m_idSetCommand;
+        }
+        public void setIdSetCommand(int idSetCommand)
+        {
+            this.m_idSetCommand = idSetCommand;
+        }
+        // [Getter/Setter] String m_idCommand
+        public int getIdCommand()
+        {
+            return this.m_idCommand;
+        }
+        public void setIdCommand(int idCommand)
+        {
+            this.m_idCommand = idCommand;
+        }
+    }
+}
