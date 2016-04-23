7d2c93593f82aa5d1f97b88640183fc6976613e8
Cassandra Yurekli <cassandra.yurekli@gmail.com>
2016-04-23 19:57:00 +0200

    début de la connection

---
+++ BAJunior/BAJunior/Controller/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAJunior.ServiceData;
+using BAJunior.Model;
+
+namespace BAJunior.Controller
+{
+    class Connection
+    {
+        public Connection() { }
+
+        public User Connect(String login, String password) {
+            User u = null;
+            UserData uData = new UserData();
+
+            return u;
+        }
+    }
+}
--- BAJunior/BAJunior/View/Forms/Connection.cs
+++ BAJunior/BAJunior/View/Forms/Connection.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BAJunior.Controller;
 
 namespace BAJunior.View.Forms
 {
@@ -15,6 +16,19 @@ namespace BAJunior.View.Forms
         public Connection()
         {
             InitializeComponent();
+            // Assign the asterisk to be the password character.
+            tb_Password.PasswordChar = '*';
+
+        }
+
+        private void btn_Connection_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tb_Password.Text) && !String.IsNullOrEmpty(tb_Login.Text)) {
+
+            } else
+            {
+                System.Windows.Forms.MessageBox.Show("Veuillez remplir tous les champs.");
+            }
         }
     }
 }
