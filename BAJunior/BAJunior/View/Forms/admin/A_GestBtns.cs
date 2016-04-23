709f1f2b02d276a00ea2cad4ac0ec56eaffab72f
Cassandra Yurekli <cassandra.yurekli@gmail.com>
2016-04-23 19:28:23 +0200

    Création des forms

---
+++ BAJunior/BAJunior/View/Forms/admin/A_GestBtns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAJunior.View.Forms.admin
+{
+    public partial class A_GestBtns : UserControl
+    {
+        public A_GestBtns()
+        {
+            InitializeComponent();
+        }
+    }
+}
