016c2091a19ac96a02c62033c773d8a9ec1f2159
mctailleux <marieclaire.tailleux@gmail.com>
2016-04-25 18:55:26 +0200

    Début Clavier

---
+++ BAJunior/BAJunior/View/Forms/user/Clavier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAJunior.View.Forms.user
+{
+    public partial class Clavier : UserControl
+    {
+        public Clavier()
+        {
+            InitializeComponent();
+        }
+    }
+}
