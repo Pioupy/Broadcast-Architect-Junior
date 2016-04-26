c85d0ccc37109e042d5a0b8855c482c68ba85f9a
mctailleux <marieclaire.tailleux@gmail.com>
2016-04-26 21:47:28 +0200

    interface GestBtns/GestUsers

--- BAJunior/BAJunior/View/Forms/admin/A_GestBtns.Designer.cs
+++ BAJunior/BAJunior/View/Forms/admin/A_GestBtns.Designer.cs
@@ -28,10 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.lv_Button = new System.Windows.Forms.ListView();
+            this.btn_DeleteBtn = new System.Windows.Forms.Button();
+            this.btn_EditBtn = new System.Windows.Forms.Button();
+            this.btn_AddBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lv_Button
+            // 
+            this.lv_Button.Location = new System.Drawing.Point(88, 110);
+            this.lv_Button.Name = "lv_Button";
+            this.lv_Button.Size = new System.Drawing.Size(493, 699);
+            this.lv_Button.TabIndex = 0;
+            this.lv_Button.UseCompatibleStateImageBehavior = false;
+            // 
+            // btn_DeleteBtn
+            // 
+            this.btn_DeleteBtn.Location = new System.Drawing.Point(874, 549);
+            this.btn_DeleteBtn.Name = "btn_DeleteBtn";
+            this.btn_DeleteBtn.Size = new System.Drawing.Size(111, 41);
+            this.btn_DeleteBtn.TabIndex = 6;
+            this.btn_DeleteBtn.Text = "Supprimer";
+            this.btn_DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // btn_EditBtn
+            // 
+            this.btn_EditBtn.Location = new System.Drawing.Point(874, 459);
+            this.btn_EditBtn.Name = "btn_EditBtn";
+            this.btn_EditBtn.Size = new System.Drawing.Size(111, 41);
+            this.btn_EditBtn.TabIndex = 5;
+            this.btn_EditBtn.Text = "Modifier";
+            this.btn_EditBtn.UseVisualStyleBackColor = true;
+            // 
+            // btn_AddBtn
+            // 
+            this.btn_AddBtn.Location = new System.Drawing.Point(874, 358);
+            this.btn_AddBtn.Name = "btn_AddBtn";
+            this.btn_AddBtn.Size = new System.Drawing.Size(111, 41);
+            this.btn_AddBtn.TabIndex = 4;
+            this.btn_AddBtn.Text = "Ajouter";
+            this.btn_AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // A_GestBtns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_DeleteBtn);
+            this.Controls.Add(this.btn_EditBtn);
+            this.Controls.Add(this.btn_AddBtn);
+            this.Controls.Add(this.lv_Button);
+            this.Name = "A_GestBtns";
+            this.Size = new System.Drawing.Size(1286, 876);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lv_Button;
+        private System.Windows.Forms.Button btn_DeleteBtn;
+        private System.Windows.Forms.Button btn_EditBtn;
+        private System.Windows.Forms.Button btn_AddBtn;
     }
 }
--- BAJunior/BAJunior/View/Forms/admin/A_GestUsers.Designer.cs
+++ BAJunior/BAJunior/View/Forms/admin/A_GestUsers.Designer.cs
@@ -28,10 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.lv_User = new System.Windows.Forms.ListView();
+            this.btn_AddUser = new System.Windows.Forms.Button();
+            this.btn_EditUser = new System.Windows.Forms.Button();
+            this.btn_DeleteUser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lv_User
+            // 
+            this.lv_User.Location = new System.Drawing.Point(78, 104);
+            this.lv_User.Name = "lv_User";
+            this.lv_User.Size = new System.Drawing.Size(487, 717);
+            this.lv_User.TabIndex = 0;
+            this.lv_User.UseCompatibleStateImageBehavior = false;
+            // 
+            // btn_AddUser
+            // 
+            this.btn_AddUser.Location = new System.Drawing.Point(888, 316);
+            this.btn_AddUser.Name = "btn_AddUser";
+            this.btn_AddUser.Size = new System.Drawing.Size(111, 41);
+            this.btn_AddUser.TabIndex = 1;
+            this.btn_AddUser.Text = "w";
+            this.btn_AddUser.UseVisualStyleBackColor = true;
+            // 
+            // btn_EditUser
+            // 
+            this.btn_EditUser.Location = new System.Drawing.Point(888, 417);
+            this.btn_EditUser.Name = "btn_EditUser";
+            this.btn_EditUser.Size = new System.Drawing.Size(111, 41);
+            this.btn_EditUser.TabIndex = 2;
+            this.btn_EditUser.Text = "Modifier";
+            this.btn_EditUser.UseVisualStyleBackColor = true;
+            // 
+            // btn_DeleteUser
+            // 
+            this.btn_DeleteUser.Location = new System.Drawing.Point(888, 507);
+            this.btn_DeleteUser.Name = "btn_DeleteUser";
+            this.btn_DeleteUser.Size = new System.Drawing.Size(111, 41);
+            this.btn_DeleteUser.TabIndex = 3;
+            this.btn_DeleteUser.Text = "Supprimer";
+            this.btn_DeleteUser.UseVisualStyleBackColor = true;
+            // 
+            // A_GestUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_DeleteUser);
+            this.Controls.Add(this.btn_EditUser);
+            this.Controls.Add(this.btn_AddUser);
+            this.Controls.Add(this.lv_User);
+            this.Name = "A_GestUsers";
+            this.Size = new System.Drawing.Size(1246, 890);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lv_User;
+        private System.Windows.Forms.Button btn_AddUser;
+        private System.Windows.Forms.Button btn_EditUser;
+        private System.Windows.Forms.Button btn_DeleteUser;
     }
 }
