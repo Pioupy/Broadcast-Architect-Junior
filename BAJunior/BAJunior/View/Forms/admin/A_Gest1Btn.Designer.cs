ad46c844e0a0487b191e7dd0a3c2d926f803293a
Pioupy <erwan.sicsic@gmail.com>
2016-05-01 17:12:07 +0200

    Debut nouveau a_gestbtn 3eme putain de fois...

--- BAJunior/BAJunior/View/Forms/admin/A_Gest1Btn.Designer.cs
+++ BAJunior/BAJunior/View/Forms/admin/A_Gest1Btn.Designer.cs
@@ -28,74 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btn_addCat = new System.Windows.Forms.Button();
+            this.cb_catName = new System.Windows.Forms.ComboBox();
+            this.tb_propName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.propNameTB = new System.Windows.Forms.TextBox();
-            this.catNameCB = new System.Windows.Forms.ComboBox();
-            this.addCatBtn = new System.Windows.Forms.Button();
-            this.ImageLV = new System.Windows.Forms.ListView();
-            this.imagePathTB = new System.Windows.Forms.TextBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.imageBrowseBtn = new System.Windows.Forms.Button();
-            this.parameterNameCB = new System.Windows.Forms.ComboBox();
-            this.addParameterBtn = new System.Windows.Forms.Button();
-            this.ParameterImagePB = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Btn_imageBrowse = new System.Windows.Forms.Button();
+            this.tb_imagePath = new System.Windows.Forms.TextBox();
+            this.lv_Image = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.rbtn_custom1 = new System.Windows.Forms.RadioButton();
+            this.rbtn_fix1 = new System.Windows.Forms.RadioButton();
+            this.rbtn_empty1 = new System.Windows.Forms.RadioButton();
+            this.tb_nom1 = new System.Windows.Forms.TextBox();
+            this.lbl_type1 = new System.Windows.Forms.Label();
+            this.tb_valor1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ParameterImagePB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.addCatBtn);
-            this.groupBox1.Controls.Add(this.catNameCB);
-            this.groupBox1.Controls.Add(this.propNameTB);
+            this.groupBox1.Controls.Add(this.btn_addCat);
+            this.groupBox1.Controls.Add(this.cb_catName);
+            this.groupBox1.Controls.Add(this.tb_propName);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(524, 32);
+            this.groupBox1.Location = new System.Drawing.Point(3, 92);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(296, 119);
+            this.groupBox1.Size = new System.Drawing.Size(383, 119);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Propriétés";
             // 
-            // groupBox2
+            // btn_addCat
             // 
-            this.groupBox2.Controls.Add(this.imageBrowseBtn);
-            this.groupBox2.Controls.Add(this.imagePathTB);
-            this.groupBox2.Controls.Add(this.ImageLV);
-            this.groupBox2.Location = new System.Drawing.Point(524, 157);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(296, 180);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Images";
+            this.btn_addCat.Location = new System.Drawing.Point(261, 78);
+            this.btn_addCat.Name = "btn_addCat";
+            this.btn_addCat.Size = new System.Drawing.Size(82, 24);
+            this.btn_addCat.TabIndex = 6;
+            this.btn_addCat.Text = "Ajouter";
+            this.btn_addCat.UseVisualStyleBackColor = true;
             // 
-            // groupBox3
+            // cb_catName
             // 
-            this.groupBox3.Controls.Add(this.ParameterImagePB);
-            this.groupBox3.Controls.Add(this.addParameterBtn);
-            this.groupBox3.Controls.Add(this.parameterNameCB);
-            this.groupBox3.Location = new System.Drawing.Point(524, 343);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(296, 136);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Paramètres";
+            this.cb_catName.FormattingEnabled = true;
+            this.cb_catName.Location = new System.Drawing.Point(91, 78);
+            this.cb_catName.Name = "cb_catName";
+            this.cb_catName.Size = new System.Drawing.Size(164, 24);
+            this.cb_catName.TabIndex = 5;
             // 
-            // label1
+            // tb_propName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nom :";
+            this.tb_propName.Location = new System.Drawing.Point(59, 35);
+            this.tb_propName.Name = "tb_propName";
+            this.tb_propName.Size = new System.Drawing.Size(196, 22);
+            this.tb_propName.TabIndex = 4;
             // 
             // label2
             // 
@@ -106,89 +102,163 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Catégorie :";
             // 
-            // propNameTB
+            // label1
             // 
-            this.propNameTB.Location = new System.Drawing.Point(59, 35);
-            this.propNameTB.Name = "propNameTB";
-            this.propNameTB.Size = new System.Drawing.Size(231, 22);
-            this.propNameTB.TabIndex = 4;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom :";
             // 
-            // catNameCB
+            // groupBox2
             // 
-            this.catNameCB.FormattingEnabled = true;
-            this.catNameCB.Location = new System.Drawing.Point(91, 78);
-            this.catNameCB.Name = "catNameCB";
-            this.catNameCB.Size = new System.Drawing.Size(164, 24);
-            this.catNameCB.TabIndex = 5;
+            this.groupBox2.Controls.Add(this.Btn_imageBrowse);
+            this.groupBox2.Controls.Add(this.tb_imagePath);
+            this.groupBox2.Controls.Add(this.lv_Image);
+            this.groupBox2.Location = new System.Drawing.Point(3, 217);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(383, 359);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Images";
             // 
-            // addCatBtn
+            // Btn_imageBrowse
             // 
-            this.addCatBtn.Location = new System.Drawing.Point(261, 78);
-            this.addCatBtn.Name = "addCatBtn";
-            this.addCatBtn.Size = new System.Drawing.Size(29, 24);
-            this.addCatBtn.TabIndex = 6;
-            this.addCatBtn.Text = "+";
-            this.addCatBtn.UseVisualStyleBackColor = true;
+            this.Btn_imageBrowse.Location = new System.Drawing.Point(302, 330);
+            this.Btn_imageBrowse.Name = "Btn_imageBrowse";
+            this.Btn_imageBrowse.Size = new System.Drawing.Size(75, 23);
+            this.Btn_imageBrowse.TabIndex = 2;
+            this.Btn_imageBrowse.Text = "Parcourir";
+            this.Btn_imageBrowse.UseVisualStyleBackColor = true;
             // 
-            // ImageLV
+            // tb_imagePath
             // 
-            this.ImageLV.Location = new System.Drawing.Point(10, 32);
-            this.ImageLV.Name = "ImageLV";
-            this.ImageLV.Size = new System.Drawing.Size(280, 97);
-            this.ImageLV.TabIndex = 0;
-            this.ImageLV.UseCompatibleStateImageBehavior = false;
+            this.tb_imagePath.Location = new System.Drawing.Point(6, 331);
+            this.tb_imagePath.Name = "tb_imagePath";
+            this.tb_imagePath.Size = new System.Drawing.Size(290, 22);
+            this.tb_imagePath.TabIndex = 1;
             // 
-            // imagePathTB
+            // lv_Image
             // 
-            this.imagePathTB.Location = new System.Drawing.Point(10, 147);
-            this.imagePathTB.Name = "imagePathTB";
-            this.imagePathTB.Size = new System.Drawing.Size(199, 22);
-            this.imagePathTB.TabIndex = 1;
+            this.lv_Image.Location = new System.Drawing.Point(10, 32);
+            this.lv_Image.Name = "lv_Image";
+            this.lv_Image.Size = new System.Drawing.Size(367, 292);
+            this.lv_Image.TabIndex = 0;
+            this.lv_Image.UseCompatibleStateImageBehavior = false;
             // 
-            // contextMenuStrip1
+            // groupBox3
             // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(67, 4);
+            this.groupBox3.Controls.Add(this.tb_valor1);
+            this.groupBox3.Controls.Add(this.lbl_type1);
+            this.groupBox3.Controls.Add(this.tb_nom1);
+            this.groupBox3.Controls.Add(this.rbtn_empty1);
+            this.groupBox3.Controls.Add(this.rbtn_fix1);
+            this.groupBox3.Controls.Add(this.rbtn_custom1);
+            this.groupBox3.Controls.Add(this.vScrollBar1);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(392, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(448, 573);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Paramètres";
             // 
-            // imageBrowseBtn
+            // pictureBox1
             // 
-            this.imageBrowseBtn.Location = new System.Drawing.Point(215, 147);
-            this.imageBrowseBtn.Name = "imageBrowseBtn";
-            this.imageBrowseBtn.Size = new System.Drawing.Size(75, 23);
-            this.imageBrowseBtn.TabIndex = 2;
-            this.imageBrowseBtn.Text = "Parcourir";
-            this.imageBrowseBtn.UseVisualStyleBackColor = true;
+            this.pictureBox1.Location = new System.Drawing.Point(124, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
-            // parameterNameCB
+            // label3
             // 
-            this.parameterNameCB.FormattingEnabled = true;
-            this.parameterNameCB.Location = new System.Drawing.Point(10, 32);
-            this.parameterNameCB.Name = "parameterNameCB";
-            this.parameterNameCB.Size = new System.Drawing.Size(245, 24);
-            this.parameterNameCB.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "1 :";
             // 
-            // addParameterBtn
+            // label4
             // 
-            this.addParameterBtn.Location = new System.Drawing.Point(261, 31);
-            this.addParameterBtn.Name = "addParameterBtn";
-            this.addParameterBtn.Size = new System.Drawing.Size(29, 24);
-            this.addParameterBtn.TabIndex = 1;
-            this.addParameterBtn.Text = "+";
-            this.addParameterBtn.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nom :";
             // 
-            // ParameterImagePB
+            // vScrollBar1
             // 
-            this.ParameterImagePB.Location = new System.Drawing.Point(10, 63);
-            this.ParameterImagePB.Name = "ParameterImagePB";
-            this.ParameterImagePB.Size = new System.Drawing.Size(280, 57);
-            this.ParameterImagePB.TabIndex = 2;
-            this.ParameterImagePB.TabStop = false;
+            this.vScrollBar1.Location = new System.Drawing.Point(424, 16);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(21, 554);
+            this.vScrollBar1.TabIndex = 2;
+            // 
+            // rbtn_custom1
+            // 
+            this.rbtn_custom1.AutoSize = true;
+            this.rbtn_custom1.Location = new System.Drawing.Point(73, 20);
+            this.rbtn_custom1.Name = "rbtn_custom1";
+            this.rbtn_custom1.Size = new System.Drawing.Size(81, 21);
+            this.rbtn_custom1.TabIndex = 3;
+            this.rbtn_custom1.TabStop = true;
+            this.rbtn_custom1.Text = "A définir";
+            this.rbtn_custom1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_fix1
+            // 
+            this.rbtn_fix1.AutoSize = true;
+            this.rbtn_fix1.Location = new System.Drawing.Point(177, 20);
+            this.rbtn_fix1.Name = "rbtn_fix1";
+            this.rbtn_fix1.Size = new System.Drawing.Size(54, 21);
+            this.rbtn_fix1.TabIndex = 4;
+            this.rbtn_fix1.TabStop = true;
+            this.rbtn_fix1.Text = "Fixe";
+            this.rbtn_fix1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_empty1
+            // 
+            this.rbtn_empty1.AutoSize = true;
+            this.rbtn_empty1.Location = new System.Drawing.Point(267, 20);
+            this.rbtn_empty1.Name = "rbtn_empty1";
+            this.rbtn_empty1.Size = new System.Drawing.Size(116, 21);
+            this.rbtn_empty1.TabIndex = 5;
+            this.rbtn_empty1.TabStop = true;
+            this.rbtn_empty1.Text = "Pas de valeur";
+            this.rbtn_empty1.UseVisualStyleBackColor = true;
+            // 
+            // tb_nom1
+            // 
+            this.tb_nom1.Location = new System.Drawing.Point(59, 49);
+            this.tb_nom1.Name = "tb_nom1";
+            this.tb_nom1.Size = new System.Drawing.Size(172, 22);
+            this.tb_nom1.TabIndex = 6;
+            // 
+            // lbl_type1
+            // 
+            this.lbl_type1.AutoSize = true;
+            this.lbl_type1.Location = new System.Drawing.Point(237, 53);
+            this.lbl_type1.Name = "lbl_type1";
+            this.lbl_type1.Size = new System.Drawing.Size(57, 17);
+            this.lbl_type1.TabIndex = 7;
+            this.lbl_type1.Text = "Valeur :";
+            // 
+            // tb_valor1
+            // 
+            this.tb_valor1.Location = new System.Drawing.Point(300, 50);
+            this.tb_valor1.Name = "tb_valor1";
+            this.tb_valor1.Size = new System.Drawing.Size(100, 22);
+            this.tb_valor1.TabIndex = 8;
             // 
             // A_Gest1Btn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -199,7 +269,8 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.ParameterImagePB)).EndInit();
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -207,19 +278,25 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox propNameTB;
+        private System.Windows.Forms.TextBox tb_propName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button addCatBtn;
-        private System.Windows.Forms.ComboBox catNameCB;
-        private System.Windows.Forms.Button imageBrowseBtn;
-        private System.Windows.Forms.TextBox imagePathTB;
-        private System.Windows.Forms.ListView ImageLV;
-        private System.Windows.Forms.PictureBox ParameterImagePB;
-        private System.Windows.Forms.Button addParameterBtn;
-        private System.Windows.Forms.ComboBox parameterNameCB;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btn_addCat;
+        private System.Windows.Forms.ComboBox cb_catName;
+        private System.Windows.Forms.Button Btn_imageBrowse;
+        private System.Windows.Forms.TextBox tb_imagePath;
+        private System.Windows.Forms.ListView lv_Image;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tb_valor1;
+        private System.Windows.Forms.Label lbl_type1;
+        private System.Windows.Forms.TextBox tb_nom1;
+        private System.Windows.Forms.RadioButton rbtn_empty1;
+        private System.Windows.Forms.RadioButton rbtn_fix1;
+        private System.Windows.Forms.RadioButton rbtn_custom1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
