ca9d8e6a358b734d29b679c7dbc48237d818aada
mctailleux <marieclaire.tailleux@gmail.com>
2016-05-06 12:31:01 +0200

    ajout Gestion Applications

--- BAJunior/BAJunior/View/Forms/admin/A_Administrator.Designer.cs
+++ BAJunior/BAJunior/View/Forms/admin/A_Administrator.Designer.cs
@@ -29,26 +29,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.GestUsers = new System.Windows.Forms.TabControl();
+            this.Administrator = new System.Windows.Forms.TabControl();
             this.GestBtns = new System.Windows.Forms.TabPage();
             this.a_GestBtns1 = new BAJunior.View.Forms.admin.A_GestBtns();
-            this.Gest_Users = new System.Windows.Forms.TabPage();
+            this.GestUsers = new System.Windows.Forms.TabPage();
             this.a_GestUsers1 = new BAJunior.View.Forms.admin.A_GestUsers();
-            this.GestUsers.SuspendLayout();
+            this.GestApps = new System.Windows.Forms.TabPage();
+            this.a_GestApps1 = new BAJunior.View.Forms.admin.A_GestApps();
+            this.Administrator.SuspendLayout();
             this.GestBtns.SuspendLayout();
-            this.Gest_Users.SuspendLayout();
+            this.GestUsers.SuspendLayout();
+            this.GestApps.SuspendLayout();
             this.SuspendLayout();
             // 
-            // GestUsers
+            // Administrator
             // 
-            this.GestUsers.Controls.Add(this.GestBtns);
-            this.GestUsers.Controls.Add(this.Gest_Users);
-            this.GestUsers.Location = new System.Drawing.Point(16, 15);
-            this.GestUsers.Margin = new System.Windows.Forms.Padding(4);
-            this.GestUsers.Name = "GestUsers";
-            this.GestUsers.SelectedIndex = 0;
-            this.GestUsers.Size = new System.Drawing.Size(1316, 943);
-            this.GestUsers.TabIndex = 0;
+            this.Administrator.Controls.Add(this.GestBtns);
+            this.Administrator.Controls.Add(this.GestUsers);
+            this.Administrator.Controls.Add(this.GestApps);
+            this.Administrator.Location = new System.Drawing.Point(16, 15);
+            this.Administrator.Margin = new System.Windows.Forms.Padding(4);
+            this.Administrator.Name = "Administrator";
+            this.Administrator.SelectedIndex = 0;
+            this.Administrator.Size = new System.Drawing.Size(1316, 943);
+            this.Administrator.TabIndex = 0;
             // 
             // GestBtns
             // 
@@ -69,17 +73,17 @@
             this.a_GestBtns1.Size = new System.Drawing.Size(1286, 876);
             this.a_GestBtns1.TabIndex = 0;
             // 
-            // Gest_Users
+            // GestUsers
             // 
-            this.Gest_Users.Controls.Add(this.a_GestUsers1);
-            this.Gest_Users.Location = new System.Drawing.Point(4, 25);
-            this.Gest_Users.Margin = new System.Windows.Forms.Padding(4);
-            this.Gest_Users.Name = "Gest_Users";
-            this.Gest_Users.Padding = new System.Windows.Forms.Padding(4);
-            this.Gest_Users.Size = new System.Drawing.Size(1308, 914);
-            this.Gest_Users.TabIndex = 1;
-            this.Gest_Users.Text = "Utilisateurs";
-            this.Gest_Users.UseVisualStyleBackColor = true;
+            this.GestUsers.Controls.Add(this.a_GestUsers1);
+            this.GestUsers.Location = new System.Drawing.Point(4, 25);
+            this.GestUsers.Margin = new System.Windows.Forms.Padding(4);
+            this.GestUsers.Name = "GestUsers";
+            this.GestUsers.Padding = new System.Windows.Forms.Padding(4);
+            this.GestUsers.Size = new System.Drawing.Size(1308, 914);
+            this.GestUsers.TabIndex = 1;
+            this.GestUsers.Text = "Utilisateurs";
+            this.GestUsers.UseVisualStyleBackColor = true;
             // 
             // a_GestUsers1
             // 
@@ -88,28 +92,49 @@
             this.a_GestUsers1.Size = new System.Drawing.Size(1246, 890);
             this.a_GestUsers1.TabIndex = 0;
             // 
+            // GestApps
+            // 
+            this.GestApps.Controls.Add(this.a_GestApps1);
+            this.GestApps.Location = new System.Drawing.Point(4, 25);
+            this.GestApps.Name = "GestApps";
+            this.GestApps.Padding = new System.Windows.Forms.Padding(3);
+            this.GestApps.Size = new System.Drawing.Size(1308, 914);
+            this.GestApps.TabIndex = 2;
+            this.GestApps.Text = "Applications";
+            this.GestApps.UseVisualStyleBackColor = true;
+            // 
+            // a_GestApps1
+            // 
+            this.a_GestApps1.Location = new System.Drawing.Point(15, 15);
+            this.a_GestApps1.Name = "a_GestApps1";
+            this.a_GestApps1.Size = new System.Drawing.Size(1280, 870);
+            this.a_GestApps1.TabIndex = 0;
+            // 
             // A_Administrator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1387, 966);
-            this.Controls.Add(this.GestUsers);
+            this.Controls.Add(this.Administrator);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "A_Administrator";
             this.Text = "Administrator";
-            this.GestUsers.ResumeLayout(false);
+            this.Administrator.ResumeLayout(false);
             this.GestBtns.ResumeLayout(false);
-            this.Gest_Users.ResumeLayout(false);
+            this.GestUsers.ResumeLayout(false);
+            this.GestApps.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl GestUsers;
+        private System.Windows.Forms.TabControl Administrator;
         private System.Windows.Forms.TabPage GestBtns;
-        private System.Windows.Forms.TabPage Gest_Users;
+        private System.Windows.Forms.TabPage GestUsers;
         private A_GestUsers a_GestUsers1;
         private A_GestBtns a_GestBtns1;
+        private System.Windows.Forms.TabPage GestApps;
+        private A_GestApps a_GestApps1;
     }
 }
