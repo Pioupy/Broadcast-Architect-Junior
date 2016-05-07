bca06b0ee223d11109c875b7a7a1fffb471143ae
mctailleux <marieclaire.tailleux@gmail.com>
2016-05-07 19:30:31 +0200

    Pop up de suppression

--- BAJunior/BAJunior/View/Forms/admin/A_GestApps.Designer.cs
+++ BAJunior/BAJunior/View/Forms/admin/A_GestApps.Designer.cs
@@ -44,41 +44,42 @@
             // 
             this.btn_DeleteApps.Location = new System.Drawing.Point(720, 500);
             this.btn_DeleteApps.Name = "btn_DeleteApps";
-            this.btn_DeleteApps.Size = new System.Drawing.Size(111, 41);
+            this.btn_DeleteApps.Size = new System.Drawing.Size(85, 33);
             this.btn_DeleteApps.TabIndex = 7;
             this.btn_DeleteApps.Text = "Supprimer";
             this.btn_DeleteApps.UseVisualStyleBackColor = true;
             // 
             // btn_EditApps
             // 
-            this.btn_EditApps.Location = new System.Drawing.Point(720, 400);
+            this.btn_EditApps.Location = new System.Drawing.Point(720, 442);
             this.btn_EditApps.Name = "btn_EditApps";
-            this.btn_EditApps.Size = new System.Drawing.Size(111, 41);
+            this.btn_EditApps.Size = new System.Drawing.Size(85, 33);
             this.btn_EditApps.TabIndex = 6;
             this.btn_EditApps.Text = "Modifier";
             this.btn_EditApps.UseVisualStyleBackColor = true;
             // 
             // btn_AddApps
             // 
-            this.btn_AddApps.Location = new System.Drawing.Point(720, 300);
+            this.btn_AddApps.Location = new System.Drawing.Point(720, 376);
             this.btn_AddApps.Name = "btn_AddApps";
-            this.btn_AddApps.Size = new System.Drawing.Size(111, 41);
+            this.btn_AddApps.Size = new System.Drawing.Size(85, 33);
             this.btn_AddApps.TabIndex = 5;
             this.btn_AddApps.Text = "Ajouter";
             this.btn_AddApps.UseVisualStyleBackColor = true;
             // 
             // lv_Apps
             // 
-            this.lv_Apps.Location = new System.Drawing.Point(80, 110);
+            this.lv_Apps.Location = new System.Drawing.Point(30, 100);
             this.lv_Apps.Name = "lv_Apps";
             this.lv_Apps.Size = new System.Drawing.Size(450, 600);
             this.lv_Apps.TabIndex = 4;
             this.lv_Apps.UseCompatibleStateImageBehavior = false;
+            this.lv_Apps.View = System.Windows.Forms.View.List;
             this.lv_Apps.SelectedIndexChanged += new System.EventHandler(this.lv_Apps_SelectedIndexChanged);
             // 
             // btn_saveApps
             // 
-            this.btn_saveApps.Location = new System.Drawing.Point(775, 213);
+            this.btn_saveApps.Location = new System.Drawing.Point(720, 221);
             this.btn_saveApps.Name = "btn_saveApps";
             this.btn_saveApps.Size = new System.Drawing.Size(85, 33);
             this.btn_saveApps.TabIndex = 10;
@@ -88,7 +89,7 @@
             // 
             // tb_NameApps
             // 
-            this.tb_NameApps.Location = new System.Drawing.Point(720, 142);
+            this.tb_NameApps.Location = new System.Drawing.Point(657, 139);
             this.tb_NameApps.Name = "tb_NameApps";
             this.tb_NameApps.Size = new System.Drawing.Size(230, 22);
             this.tb_NameApps.TabIndex = 9;
@@ -96,7 +97,7 @@
             // l_application
             // 
             this.l_application.AutoSize = true;
-            this.l_application.Location = new System.Drawing.Point(548, 145);
+            this.l_application.Location = new System.Drawing.Point(504, 142);
             this.l_application.Name = "l_application";
             this.l_application.Size = new System.Drawing.Size(147, 17);
             this.l_application.TabIndex = 8;
@@ -104,7 +105,7 @@
             // 
             // tb_ApplicationFolder
             // 
-            this.tb_ApplicationFolder.Location = new System.Drawing.Point(715, 173);
+            this.tb_ApplicationFolder.Location = new System.Drawing.Point(657, 173);
             this.tb_ApplicationFolder.Name = "tb_ApplicationFolder";
             this.tb_ApplicationFolder.Size = new System.Drawing.Size(230, 22);
             this.tb_ApplicationFolder.TabIndex = 11;
@@ -112,7 +113,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(548, 176);
+            this.label1.Location = new System.Drawing.Point(486, 176);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(161, 17);
             this.label1.TabIndex = 12;
@@ -120,7 +121,7 @@
             // 
             // btn_FindApplication
             // 
-            this.btn_FindApplication.Location = new System.Drawing.Point(956, 173);
+            this.btn_FindApplication.Location = new System.Drawing.Point(906, 173);
             this.btn_FindApplication.Name = "btn_FindApplication";
             this.btn_FindApplication.Size = new System.Drawing.Size(75, 23);
             this.btn_FindApplication.TabIndex = 13;
---
+++ BAJunior/BAJunior/View/Forms/admin/Delete.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BAJunior.View.Forms.admin
+{
+    partial class Suppression
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_DeleteYes = new System.Windows.Forms.Button();
+            this.btn_DeleteNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voulez-vous vraiment supprimer cet élément ?";
+            // 
+            // btn_DeleteYes
+            // 
+            this.btn_DeleteYes.Location = new System.Drawing.Point(86, 116);
+            this.btn_DeleteYes.Name = "btn_DeleteYes";
+            this.btn_DeleteYes.Size = new System.Drawing.Size(75, 23);
+            this.btn_DeleteYes.TabIndex = 1;
+            this.btn_DeleteYes.Text = "Oui";
+            this.btn_DeleteYes.UseVisualStyleBackColor = true;
+            // 
+            // btn_DeleteNo
+            // 
+            this.btn_DeleteNo.Location = new System.Drawing.Point(245, 116);
+            this.btn_DeleteNo.Name = "btn_DeleteNo";
+            this.btn_DeleteNo.Size = new System.Drawing.Size(75, 23);
+            this.btn_DeleteNo.TabIndex = 2;
+            this.btn_DeleteNo.Text = "Non";
+            this.btn_DeleteNo.UseVisualStyleBackColor = true;
+            // 
+            // Suppression
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 187);
+            this.Controls.Add(this.btn_DeleteNo);
+            this.Controls.Add(this.btn_DeleteYes);
+            this.Controls.Add(this.label1);
+            this.Name = "Suppression";
+            this.Text = "Delete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_DeleteYes;
+        private System.Windows.Forms.Button btn_DeleteNo;
+    }
+}
